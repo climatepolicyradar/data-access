358a7123f0aa389c6f71c24a25f36f4bf6b49d12
mpjuhasz <juhaszmp@gmail.com>
2023-10-16 15:10:34 +0200

    test added

--- tests/test_models.py
+++ tests/test_models.py
@@ -181,6 +181,16 @@ def test_dataset_filter_by_language(test_dataset):
     assert dataset.documents[1].languages == ["en"]
 
 
+def test_dataset_filter_by_corpus(test_dataset):
+    """Test Dataset.filter_by_corpus"""
+    dataset = test_dataset.filter_by_corpus('UNFCCC')
+
+    assert len(dataset) == 0
+
+    dataset = test_dataset.filter_by_corpus('CCLW')
+
+    assert len(dataset) == 3
+
 def test_dataset_get_all_text_blocks(test_dataset):
     text_blocks = test_dataset.get_all_text_blocks()
     num_text_blocks = sum(
