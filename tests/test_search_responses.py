75f5ee3193bd34a18bda4d30e644ddd100db0071
harrisonpim <hello@harrisonpim.com>
2023-11-06 10:44:16 +0000

    test whether an empty response is parsed correctly

--- tests/test_search_responses.py
+++ tests/test_search_responses.py
@@ -18,6 +18,15 @@ def valid_vespa_search_response():
 
 
 @pytest.fixture
+def empty_vespa_search_response():
+    with open("tests/test_data/search_responses/empty_search_response.json") as f:
+        response_json = json.load(f)
+        return VespaResponse(
+            json=response_json, status_code=200, url="", operation_type=""
+        )
+
+
+@pytest.fixture
 def invalid_vespa_search_response():
     return VespaResponse(
         json={"this json": "is not valid"}, status_code=500, url="", operation_type=""
@@ -138,3 +147,15 @@ def test_whether_valid_document_id_is_correctly_split():
 def test_whether_invalid_document_id_raises_value_error():
     with pytest.raises(ValueError):
         split_document_id("this is not a valid document id")
+
+
+def test_whether_empty_response_is_parsed_correctly(empty_vespa_search_response):
+    request = SearchParameters(
+        query_string="ThisStringShouldNotMatchAnything", exact_match=True
+    )
+    response = parse_vespa_response(
+        request=request, vespa_response=empty_vespa_search_response
+    )
+    assert response.total_hits == 0
+    assert response.families == []
+    assert response.continuation_token is None
