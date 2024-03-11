820efe2cfe59f82be3d0aa5fb70d99b6d452fa80
olaughter <fred@climatepolicyradar.org>
2024-03-11 14:34:06 +0000

    Add dig function for vespa responses
    Vespa objects are a bit gnarly and navigating them safely ends up with
    a noisy chain of `.get()` calls. This function aims to improve the
    situation slightly

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from cpr_data_access.utils import is_sensitive_query, load_sensitive_query_terms
+from cpr_data_access.utils import dig, is_sensitive_query, load_sensitive_query_terms
 
 
 TEST_SENSITIVE_QUERY_TERMS = (
@@ -38,3 +38,30 @@ def test_load_sensitive_query_terms():
     terms = load_sensitive_query_terms()
     assert terms
     assert len(terms) > 2000
+
+
+@pytest.mark.parametrize(
+    "fields, default, expected",
+    [
+        (["field"], None, "parent"),
+        (["children", 0, "name"], None, "child_one"),
+        (["children", 1, "items", 2], None, "two"),
+        (["children", 2, "sub", "sub_sub", "sub_sub_sub", 2], None, "c"),
+        (["children", 5], "default_1", "default_1"),
+        (["children", 2, "sub", "sub_sub", "NO"], "default_2", "default_2"),
+    ],
+)
+def test_dig(fields, default, expected):
+    obj = {
+        "field": "parent",
+        "children": [
+            {"name": "child_one"},
+            {"name": "child_two", "items": ["zero", "one", "two"]},
+            {
+                "name": "child_three",
+                "sub": {"sub_sub": {"sub_sub_sub": ["a", "b", "c"]}},
+            },
+        ],
+    }
+
+    assert dig(obj, *fields, default=default) == expected
