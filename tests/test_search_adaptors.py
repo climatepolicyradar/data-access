d2ff4a0b049998023493c360d67307541c074e9c
olaughter <fred@climatepolicyradar.org>
2024-03-14 12:10:39 +0000

    Add test coverage for user input and query types

--- tests/test_search_adaptors.py
+++ tests/test_search_adaptors.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 from cpr_data_access.search_adaptors import VespaSearchAdapter
@@ -65,3 +67,59 @@ def test_vespa_search_adaptor__document_ids(fake_vespa_credentials, document_ids
     got_document_ids = list(set(got_document_ids))
 
     assert sorted(got_document_ids) == sorted(document_ids)
+
+
+@pytest.mark.vespa
+def test_vespa_search_adaptor__bad_query_string_still_works(fake_vespa_credentials):
+    family_name = ' Bad " query/    '
+    request = SearchParameters(query_string=family_name)
+    try:
+        vespa_search(fake_vespa_credentials, request)
+    except Exception as e:
+        assert False, f"failed with: {e}"
+
+
+@pytest.mark.vespa
+def test_vespa_search_adaptor__hybrid(fake_vespa_credentials):
+    family_name = "Climate Change Adaptation and Low Emissions Growth Strategy by 2035"
+    request = SearchParameters(query_string=family_name)
+    response = vespa_search(fake_vespa_credentials, request)
+
+    # Was the family searched for in the results.
+    # Note that this is a fairly loose test
+    got_family_names = []
+    for fam in response.families:
+        for doc in fam.hits:
+            got_family_names.append(doc.family_name)
+    assert family_name in got_family_names
+
+
+@pytest.mark.vespa
+def test_vespa_search_adaptor__exact(fake_vespa_credentials):
+    query_string = "Environmental Strategy for 2014-2023"
+    request = SearchParameters(query_string=query_string, exact_match=True)
+    response = vespa_search(fake_vespa_credentials, request)
+    got_family_names = []
+    for fam in response.families:
+        for doc in fam.hits:
+            got_family_names.append(doc.family_name)
+    # For an exact query where this term only exists in the family name, we'd expect
+    # it to be the only result so can be quite specific
+    assert len(set(got_family_names)) == 1
+    assert got_family_names[0] == query_string
+
+    # Conversely we'd expect nothing if the query string isnt present
+    query_string = "no such string as this can be found in the test documents"
+    request = SearchParameters(query_string=query_string, exact_match=True)
+    response = vespa_search(fake_vespa_credentials, request)
+    assert len(response.families) == 0
+
+
+@pytest.mark.vespa
+@patch("cpr_data_access.vespa.SENSITIVE_QUERY_TERMS", {"Government"})
+def test_vespa_search_adaptor__sensitive(fake_vespa_credentials):
+    request = SearchParameters(query_string="Government")
+    response = vespa_search(fake_vespa_credentials, request)
+
+    # Without being too prescriptive, we'd expect something back for this
+    assert len(response.families) > 0
